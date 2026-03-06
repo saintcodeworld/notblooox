7aa9305f0040413e5203b1f5118931187e8aacae
saintcodeworld <sainttwww@gmail.com>
2026-03-06 23:32:32 +0400

    Replace generic X icon with official Twitter/X logo

--- front/components/Navbar.tsx
+++ front/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import { X } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Navbar() {
@@ -25,7 +24,14 @@ export default function Navbar() {
         {/* Social Icons (X/Twitter) */}
         <div className="hidden md:flex space-x-4">
           <a href="https://x.com/i/communities/2030002380419518556" target="_blank" rel="noopener noreferrer">
-            <X className="h-6 w-6 text-gray-800 cursor-pointer hover:opacity-70 transition-opacity" />
+            <svg 
+              className="h-6 w-6 text-gray-800 cursor-pointer hover:opacity-70 transition-opacity" 
+              viewBox="0 0 24 24" 
+              fill="currentColor"
+              aria-label="X (Twitter)"
+            >
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+            </svg>
           </a>
         </div>
       </div>
